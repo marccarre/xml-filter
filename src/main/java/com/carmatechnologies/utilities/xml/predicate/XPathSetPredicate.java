7ca3d8f32f015920009a00de06cff28c0d679ce9
Marc CARRE <carre.marc@gmail.com>
2015-08-26 06:56:23 +0100

    Added XPathSetPredicate (+tests).

---
+++ src/main/java/com/carmatechnologies/utilities/xml/predicate/XPathSetPredicate.java
@@ -0,0 +1,58 @@
+package com.carmatechnologies.utilities.xml.predicate;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Sets;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public final class XPathSetPredicate extends AbstractXPathPredicate implements Predicate<Node> {
+    private final Set<String> whiteList;
+
+    public XPathSetPredicate(final XPathExpression xpathExpression, final Set<String> whiteList) {
+        super(xpathExpression);
+        this.whiteList = checkSet(whiteList);
+    }
+
+    public XPathSetPredicate(final String xpathQuery, final Set<String> whiteList) throws XPathExpressionException {
+        super(xpathQuery);
+        this.whiteList = checkSet(whiteList);
+    }
+
+    private Set<String> checkSet(final Set<String> whiteList) {
+        checkNotNull(whiteList, "White-list set must NOT be null.");
+        final Set<String> sanitizedWhiteList = sanitize(whiteList);
+        checkArgument(!sanitizedWhiteList.isEmpty(), "White-list set must NOT be empty.");
+        return sanitizedWhiteList;
+    }
+
+    private Set<String> sanitize(final Set<String> whiteList) {
+        final Set<String> sanitizedWhiteList = Sets.newHashSetWithExpectedSize(whiteList.size());
+        for (final String element : whiteList) {
+            if (element == null)
+                continue;
+            final String sanitizedElement = element.trim();
+            if (sanitizedElement.isEmpty())
+                continue;
+            sanitizedWhiteList.add(sanitizedElement);
+        }
+        return sanitizedWhiteList;
+    }
+
+    @Override
+    protected boolean condition(final NodeList matchedNodes) {
+        for (int i = 0; i < matchedNodes.getLength(); ++i) {
+            final Node node = matchedNodes.item(i);
+            final String text = node.getTextContent().trim();
+            if (whiteList.contains(text))
+                return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/com/carmatechnologies/utilities/xml/predicate/XPathSetPredicateTest.java
@@ -0,0 +1,71 @@
+package com.carmatechnologies.utilities.xml.predicate;
+
+import com.google.common.collect.Sets;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.w3c.dom.Node;
+
+import javax.xml.xpath.XPathExpressionException;
+import java.util.Set;
+
+import static com.carmatechnologies.utilities.xml.TestingUtilities.parseDomTree;
+import static com.carmatechnologies.utilities.xml.TestingUtilities.streamFor;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class XPathSetPredicateTest {
+
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
+    private final Node domTree = parseDomTree(streamFor("/books.xml"));
+
+    @Test
+    public void xpathSetPredicateShouldReturnTrueWhenXPathQueryFindsElementsFromSet() throws XPathExpressionException {
+        Set<String> whiteList = Sets.newHashSet("magician", "xml");
+        assertThat(new XPathSetPredicate("//book/tags/tag/text()", whiteList).apply(domTree), is(true));
+        assertThat(new XPathSetPredicate("//book/descendant::*/text()", whiteList).apply(domTree), is(true));
+    }
+
+    @Test
+    public void xpathSetPredicateShouldReturnFalseWhenXPathQueryFindsElementsButTheseAreNotInSet() throws XPathExpressionException {
+        Set<String> badWhiteList = Sets.newHashSet("non-existant", "not here!");
+        assertThat(new XPathSetPredicate("//book/tags/tag/text()", badWhiteList).apply(domTree), is(false));
+        assertThat(new XPathSetPredicate("//book/descendant::*/text()", badWhiteList).apply(domTree), is(false));
+    }
+
+    @Test
+    public void xpathSetPredicateShouldReturnFalseWhenXPathQueryDoesNotFindAnyElement() throws XPathExpressionException {
+        Set<String> whiteList = Sets.newHashSet("magician", "xml");
+        assertThat(new XPathSetPredicate("//non-existant/text()", whiteList).apply(domTree), is(false));
+        assertThat(new XPathSetPredicate("//non-existant/descendant::*/text()", whiteList).apply(domTree), is(false));
+    }
+
+    @Test
+    public void xpathSetPredicateShouldBeReusable() throws XPathExpressionException {
+        Set<String> whiteList = Sets.newHashSet("magician", "xml");
+        final XPathSetPredicate predicate = new XPathSetPredicate("//book/tags/tag/text()", whiteList);
+
+        assertThat(predicate.apply(domTree), is(true));
+        assertThat(predicate.apply(domTree), is(true)); // Evaluate another time...
+        assertThat(predicate.apply(domTree), is(true)); // Evaluate yet another time...
+    }
+
+    @Test
+    public void xpathSetPredicateShouldSanitizeInputAndReturnTrueWhenXPathQueryFindsElementsFromSet() throws XPathExpressionException {
+        Set<String> whiteList = Sets.newHashSet("  magician  ", "     xml    ", null, "", "   ");
+        assertThat(new XPathSetPredicate("//book/tags/tag/text()", whiteList).apply(domTree), is(true));
+        assertThat(new XPathSetPredicate("//book/descendant::*/text()", whiteList).apply(domTree), is(true));
+    }
+
+    @Test
+    public void xpathSetPredicateShouldSanitizeInputAndThrowIllegalArgumentExceptionIfSetIsEmptyAfterSanitization() throws XPathExpressionException {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(equalTo("White-list set must NOT be empty."));
+
+        Set<String> whiteList = Sets.newHashSet("  ", null, "", "      ");
+        new XPathSetPredicate("//book/tags/tag/text()", whiteList);
+    }
+}
