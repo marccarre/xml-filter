6c3e83920a0a1fc5369ca8e3b76a278678f2f4b5
Marc CARRE <carre.marc@gmail.com>
2015-08-26 06:02:09 +0100

    Factorized XPathPredicate's boilerplate in Template Method abstract parent class.

---
+++ src/main/java/com/carmatechnologies/utilities/xml/predicate/AbstractXPathPredicate.java
@@ -0,0 +1,40 @@
+package com.carmatechnologies.utilities.xml.predicate;
+
+import com.google.common.base.Predicate;
+import org.w3c.dom.Node;
+
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Abstract parent class for XPath predicates, following the "Template Method" pattern:
+ * - factorizes boilerplate, and
+ * - defines {@link AbstractXPathPredicate#condition condition} as a method to implement in child classes.
+ */
+public abstract class AbstractXPathPredicate implements Predicate<Node> {
+    private final XPathExpression xpathExpression;
+
+    public AbstractXPathPredicate(final XPathExpression xpathExpression) {
+        this.xpathExpression = checkNotNull(xpathExpression, "XPathExpression must NOT be null.");
+    }
+
+    public AbstractXPathPredicate(final String xpathQuery) throws XPathExpressionException {
+        this(XPathFactory.newInstance().newXPath().compile(checkNotNull(xpathQuery, "XPath query must NOT be null.")));
+    }
+
+    @Override
+    public boolean apply(final Node domTree) {
+        checkNotNull(domTree, "Node must NOT be null.");
+        try {
+            final String result = xpathExpression.evaluate(domTree);
+            return condition(result);
+        } catch (XPathExpressionException e) {
+            throw new RuntimeException("Failed to evaluate XPath expression on DOM tree.", e);
+        }
+    }
+
+    protected abstract boolean condition(final String result);
+}
--- src/main/java/com/carmatechnologies/utilities/xml/predicate/XPathPredicate.java
+++ src/main/java/com/carmatechnologies/utilities/xml/predicate/XPathPredicate.java
@@ -5,29 +5,19 @@ import org.w3c.dom.Node;
 
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-public final class XPathPredicate implements Predicate<Node> {
-    private final XPathExpression xpathExpression;
+public final class XPathPredicate extends AbstractXPathPredicate implements Predicate<Node> {
 
     public XPathPredicate(final XPathExpression xpathExpression) {
-        this.xpathExpression = checkNotNull(xpathExpression, "XPathExpression must NOT be null.");
+        super(xpathExpression);
     }
 
     public XPathPredicate(final String xpathQuery) throws XPathExpressionException {
-        this(XPathFactory.newInstance().newXPath().compile(checkNotNull(xpathQuery, "XPath query must NOT be null.")));
+        super(xpathQuery);
     }
 
     @Override
-    public boolean apply(final Node domTree) {
-        checkNotNull(domTree, "Node must NOT be null.");
-        try {
-            final String result = xpathExpression.evaluate(domTree);
-            return !result.isEmpty();
-        } catch (XPathExpressionException e) {
-            throw new RuntimeException("Failed to evaluate XPath expression on DOM tree.", e);
-        }
+    protected boolean condition(final String result) {
+        return !result.isEmpty();
     }
 }
